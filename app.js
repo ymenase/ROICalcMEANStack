c687c31b119fab1d350a87e2e70c0dde723e2136
Yvette Menase <yvette.j.menase@gmail.com>
2017-07-18 20:16:02 -0600

    added db.js

--- app.js
+++ app.js
@@ -3,76 +3,8 @@
  * Module dependencies
  */
 
- //Import the mongoose module
- var mongoose = require('mongoose')
- //Set up default mongoose connection
- var mongoDB = 'mongodb://localhost/test';
- mongoose.connect(mongoDB);
- //Get the default connection
- var db = mongoose.connection;
 
- //Bind connection to error event (to get notification of connection errors)
- db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-
-//Define Schema
-var Schema = mongoose.Schema;
-
-var accountingSchema = new Schema({
-
-  revenue: {
-    name : String,
-    oneTimePmt: Number,
-    monthlyPmt: Number
-  },
-
-  expenses: {
-    name: String,
-    oneTimePmt: Number,
-    monthlyPmt: Number
-  }
-
-})
-
-//Compile model from Schema
-var accountingModel = mongoose.model('accountingModel', accountingSchema);
-
-var testInstance = new accountingModel({
-  revenue: {
-    name: 'groceries',
-    oneTimePmt: 500,
-    monthlyPmt: 20
-  }
-});
-
-testInstance.save(function (err) {
-  if (err) return handleError(err);
-  // saved!
-});
-
-var testInstance2 = new accountingModel({
-  revenue: {
-    name: 'poo',
-    oneTimePmt: 50,
-    monthlyPmt: 0
-  }
-});
-
-testInstance2.save(function (err) {
-  if (err) return handleError(err);
-  // saved!
-});
-
-
-
-var testPrint = accountingModel.find({"revenue" : {"name" : "poo", "oneTimePmt" : "50", "monthlyPmt" : "0"} }, "revenue.name", function (err, athletes) {
-  if (err) return handleError(err);
-  // 'athletes' contains the list of athletes that match the criteria.
-});
-
-console.log("********************************\n**************\n" + testPrint);
-
-
-
+require( './db' );
 
 var express = require('express'),
   bodyParser = require('body-parser'),
